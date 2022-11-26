1821df79ad3bdd29a7051df33e4c42821ea7f423
Marcela Pinheiro Proença <104868735+marcelaproencaeng@users.noreply.github.com>
2022-11-25 22:39:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./styles.css";
+const Personagens = () => {};
+export default function App() {
+  const personagens = [
+    "Jill Valentine",
+    "Jack Baker",
+    "Claire Redfield",
+    "Ada Wong",
+    "Chris Redfield",
+    "Leon S. Kennedy",
+    "Albert Wesker",
+    "Lucas Baker"
+  ];
+  const [nome, setNome] = useState("");
+  function onClickRedfield() {}
+  function onClickBaker() {}
+  return (
+    <div>
+      <h3>Personagens de Resident Evil</h3>
+      <button onClick={onClickRedfield}>listar Redfield</button>
+
+      <button onClick={onClickBaker}>listar Baker</button>
+      <ul>
+        {personagens
+          .filter(() => true)
+          .map((personagem) => (
+            <li>{personagem}</li>
+          ))}
+      </ul>
+    </div>
+  );
+}
